c0ef2a335c7b1a0a82e5ca30ca880492f3044807
sys-cat <systemcat91@gmail.com>
2017-11-27 06:53:58 +0900

    add menthon

--- cmd/slackbot/main.go
+++ cmd/slackbot/main.go
@@ -3,8 +3,14 @@ package main
 import (
 	"log"
 	"os"
+	"strings"
 
 	"github.com/nlopes/slack"
+)
+
+var (
+	botId   string
+	botName string
 )
 
 func run(api *slack.Client) int {
@@ -17,9 +23,18 @@ func run(api *slack.Client) int {
 			switch ev := msg.Data.(type) {
 			case *slack.HelloEvent:
 				log.Print("Hello Event")
+			case *slack.ConnectedEvent:
+				botId = ev.Info.User.ID
+				botName = ev.Info.User.Name
 			case *slack.MessageEvent:
 				log.Print("Message: %v\n", ev)
-				rtm.SendMessage(rtm.NewOutgoingMessage("Hello world", ev.Channel))
+				user := ev.User
+				text := ev.Text
+				channel := ev.Channel
+				if ev.Type == "message" && strings.HasPrefix(text, "<@"+botId+">") {
+					rtm.handleResponse(user, text, channel)
+				}
+				//rtm.SendMessage(rtm.NewOutgoingMessage("Hello world", ev.Channel))
 			case *slack.InvalidAuthEvent:
 				log.Print("Invalid credentials")
 				return 1
