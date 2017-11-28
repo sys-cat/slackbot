52f6dc747b5e5a7e23a8b7418b29ab132a8fbe8f
sys-cat <systemcat91@gmail.com>
2017-11-29 03:12:55 +0900

    not use select, send connection message on 'bot' channel, echo type message on slack

--- cmd/slackbot/main.go
+++ cmd/slackbot/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
-	"strings"
 
 	"github.com/nlopes/slack"
 )
@@ -17,30 +17,37 @@ func run(api *slack.Client) int {
 	rtm := api.NewRTM()
 	go rtm.ManageConnection()
 
-	for {
-		select {
-		case msg := <-rtm.IncomingEvents:
-			switch ev := msg.Data.(type) {
-			case *slack.HelloEvent:
-				log.Print("Hello Event")
-			case *slack.ConnectedEvent:
-				botId = ev.Info.User.ID
-				botName = ev.Info.User.Name
-			case *slack.MessageEvent:
-				log.Print("Message: %v\n", ev)
-				user := ev.User
-				text := ev.Text
-				channel := ev.Channel
-				if ev.Type == "message" && strings.HasPrefix(text, "<@"+botId+">") {
-					rtm.handleResponse(user, text, channel)
+	for msg := range rtm.IncomingEvents {
+		switch ev := msg.Data.(type) {
+		case *slack.HelloEvent:
+			log.Print("Hello Event")
+		case *slack.ConnectedEvent:
+			bot := ""
+			for _, channel := range ev.Info.Channels {
+				if channel.Name == "bot" {
+					bot = channel.ID
 				}
-				//rtm.SendMessage(rtm.NewOutgoingMessage("Hello world", ev.Channel))
-			case *slack.InvalidAuthEvent:
-				log.Print("Invalid credentials")
-				return 1
 			}
+			log.Printf("URL: %s\n", ev.Info.URL)
+			rtm.SendMessage(rtm.NewOutgoingMessage("connection bot", bot))
+		case *slack.MessageEvent:
+			// TODO : 自分自身をGlobalな変数で持つ
+			// TODO : 自分にメンションが来た時だけ反応する
+			// TODO : 自分が呼ばれたチャンネルを持つ
+			log.Printf("Channel: %s, Message: %s\n", ev.Msg.Channel, ev.Msg.Text)
+			mess := ev.Msg.Text
+			rtm.SendMessage(rtm.NewOutgoingMessage(fmt.Sprintf("Message is \"%s\"", mess), ev.Msg.Channel))
+		case *slack.PresenceChangeEvent:
+			log.Printf("PresenceChangeEvent: %v\n", ev)
+		case *slack.RTMError:
+			log.Printf("Error: %s\n", ev.Error())
+		case *slack.InvalidAuthEvent:
+			log.Print("Invalid credentials")
+			return 1
+		default:
 		}
 	}
+	return 1
 }
 
 func main() {
@@ -49,5 +56,6 @@ func main() {
 		log.Fatal("missing API token")
 	}
 	api := slack.New(token)
-	os.Exit(run(api))
+	//os.Exit(run(api))
+	run(api)
 }
